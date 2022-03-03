349f0d7a88ae22955a0e91ef97d8bc99561e6f4b
Yanone <post@yanone.de>
2022-03-03 15:56:54 +0100

    Package

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+install_requires = [  # I get to this in a second
+    "gftools",
+]
+
+setup(
+    name="fontproduction",
+    version="1.0",
+    description="",
+    author="Yanone",
+    author_email="post@yanone.de",
+    url="https://gthub.com/yanone",
+    install_requires=install_requires,
+    package_dir={"": "Lib"},
+    packages=find_packages("Lib"),
+    include_package_data=True,
+)
